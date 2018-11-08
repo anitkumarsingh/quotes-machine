57ef4e181cd14b86aae2a27029ea1dba286b40b6
Anitkumarsingh <rockymaster118@gmail.com>
2018-11-08 18:19:49 +0530

    finised project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor(){
     super();
     this.state ={
-     quote:''
+     quote:'',
+     author: ' '
     }
   }
   
@@ -13,7 +14,11 @@ class App extends Component {
       fetch('https://talaikis.com/api/quotes/random/')
       .then(response => response.json())
       .then(data =>{
-        this.setState({quote:data})
+        this.setState({
+          quote:data.quote,
+          author:data.author
+        })
+        
         }
       )
       .catch(err =>{
@@ -25,16 +30,23 @@ class App extends Component {
     // e.preventDafault();
    this.componentDidMount();
   }
-  twitterHandler = ()=>{
-    console.log("clicked")
+  twitterHandler = (text,author)=>{
+    console.log(text);
+    console.log(author);
+  // var text = this.state.quote;
+  // var author = this.state.author;
+    window.open('http://twitter.com/share?url='+encodeURIComponent()+'&text='+encodeURIComponent(text)+'&author='+encodeURIComponent(author), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+
   }
   render() {
     console.log(this.state.quote);
     return (
       <div className="App" id="quote-box">
-       <p id="text">{this.state.quote.quote}</p>
-       <p id="author">{this.state.quote.author}</p>
-       <code code="" style={{boxSizing:'inherit',marginBottom: '0px'}}onClick={this.twitterHandler}>Twitter</code>
+       <p id="text">{this.state.quote}</p>
+       <p id="author">{this.state.author}</p>
+       <div>
+         <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank" onClick={()=>this.twitterHandler(this.state.quote,this.state.author)}>Twitter</a>
+       </div>
        <button id="new-quote" onClick={this.nextQuoteHandler}>New Quotes</button>
       </div>
     );
